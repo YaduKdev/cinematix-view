8ab7cb4c969a02102eb0b5080f9fc376a1e31035
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2024-09-19 19:26:29 +0530

    add navigation page. add links functionality and add routes. add autocomplete functionality and add autocomplete api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header";
+import Home from "./Routes/Home/Home";
+import Movies from "./Routes/Movies/Movies";
+import Admin from "./Routes/Admin/Admin";
+import Auth from "./Routes/Auth/Auth";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Header />
+      <section>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </section>
+    </>
+  );
 }
 
 export default App;
