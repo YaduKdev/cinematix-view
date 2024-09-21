568f0d01037f629749fe3167eb39966d02da94b3
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2024-09-22 02:30:08 +0530

    add view all btn to carousel section. make carousel section responsive.

--- src/Components/HeroCarousel.jsx
+++ src/Components/HeroCarousel.jsx
@@ -4,27 +4,47 @@ import img2 from "../assets/2.jpg";
 import img3 from "../assets/3.jpg";
 import img4 from "../assets/4.jpg";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectCoverflow, Navigation, Pagination } from "swiper/modules";
-
+import { EffectCoverflow } from "swiper/modules";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const HeroCarousel = () => {
   return (
     <Box margin="auto" width={"100%"} paddingTop={"100px"}>
-      <Box margin="auto">
-        <Typography
-          gutterBottom={true}
-          align="center"
-          variant="h4"
-          color="primary"
-        >
+      <Box
+        margin="auto"
+        sx={{
+          position: "relative",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Typography gutterBottom={true} variant="h4" color="primary">
           LATEST RELEASES
         </Typography>
+        <Button
+          LinkComponent={Link}
+          to="/movies"
+          sx={{
+            position: {
+              xs: "relative",
+              lg: "absolute",
+            },
+            top: "0",
+            right: "20px",
+          }}
+          variant="outlined"
+          size="large"
+        >
+          VIEW ALL
+        </Button>
       </Box>
       <Box width={"100%"} margin={"50px auto 100px"}>
         <Swiper
