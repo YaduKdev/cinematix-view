d89d2ada48f9f7119a5aa48418f5abbd421587e1
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2024-09-21 21:13:14 +0530

    add notification button to hero poster.

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,13 +1,41 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, IconButton, Tooltip } from "@mui/material";
+import React, { useState } from "react";
 import HeroCarousel from "../../Components/HeroCarousel";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 
 import "./Home.css";
 
 const Home = () => {
+  const [notification, setNotification] = useState(false);
+
+  const notify = (
+    <Tooltip title="Set Notification">
+      <IconButton>
+        <NotificationsIcon fontSize="large" sx={{ color: "#FAFAFA" }} />
+      </IconButton>
+    </Tooltip>
+  );
+
+  const notified = (
+    <Tooltip title="Remove Notification">
+      <IconButton>
+        <NotificationsActiveIcon fontSize="large" color="primary" />
+      </IconButton>
+    </Tooltip>
+  );
+
   return (
     <>
       <div className="hero">
+        <div className="notifications-container">
+          <div
+            className="notifications"
+            onClick={() => setNotification(!notification)}
+          >
+            {notification ? notified : notify}
+          </div>
+        </div>
         <div className="hero-text-container">
           <div className="hero-text"></div>
         </div>
