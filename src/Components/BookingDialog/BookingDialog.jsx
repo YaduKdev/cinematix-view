7e7957c65baf8d60f2124ad3ca1c501ae84219c6
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2024-11-10 18:26:27 +0530

    bookingdialog almost complete. Payment gateway and backend integration remaining.

--- src/Components/BookingDialog/BookingDialog.jsx
+++ src/Components/BookingDialog/BookingDialog.jsx
@@ -15,7 +15,17 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormControl from "@mui/material/FormControl";
 import FormLabel from "@mui/material/FormLabel";
-import { Box } from "@mui/material";
+import { Box, Divider } from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import dayjs from "dayjs";
+
+import EventSeatIcon from "@mui/icons-material/EventSeat";
+import ChairIcon from "@mui/icons-material/Chair";
+import WeekendIcon from "@mui/icons-material/Weekend";
 
 import "./BookingDialog.css";
 
@@ -28,12 +38,48 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   },
 }));
 
-const steps = ["Select City", "Select Cinema", "Book Seats", "Payment"];
+const steps = [
+  "Select City",
+  "Select Cinema",
+  "Select Date",
+  "Pick Seats",
+  "Payment",
+];
+
+const timings = [
+  "10:45 PM",
+  "01:15 PM",
+  "03:45 PM",
+  "06:15 PM",
+  "09:00 PM",
+  "11:30 PM",
+];
+
+const seatAvailability = [
+  {
+    condition: "Available",
+    color: "#BD1616",
+  },
+  {
+    condition: "Selected",
+    color: "#FFA726",
+  },
+  {
+    condition: "Booked",
+    color: "#9F9F9F",
+  },
+];
+
+const tomorrow = dayjs().add(7, "day");
 
 const BookingDialog = ({ open, handleClose, movie }) => {
   const [activeStep, setActiveStep] = useState(0);
   const [city, setCity] = useState();
   const [cinema, setCinema] = useState();
+  const [date, setDate] = useState();
+  const [showTime, setShowTime] = useState();
+  const [openDate, setOpenDate] = useState(false);
+  const [bookedSeats, setBookedSeats] = useState([]);
 
   const setCurrentCity = (event) => {
     const { value } = event.target;
@@ -57,14 +103,45 @@ const BookingDialog = ({ open, handleClose, movie }) => {
     setActiveStep((prevActiveStep) => prevActiveStep + 1);
   };
 
+  const seatBookingHandler = (event) => {
+    const { value, checked } = event.target;
+    let seatsArray = [...bookedSeats];
+
+    if (checked) {
+      seatsArray.push(value);
+      setBookedSeats(seatsArray);
+    }
+
+    if (!checked) {
+      seatsArray.splice(seatsArray.indexOf(value), 1);
+      setBookedSeats(seatsArray);
+    }
+  };
+
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
     if (activeStep === 1) {
-      setCinema();
       setCity();
+      setCinema();
+      setDate();
+      setShowTime();
+      setBookedSeats([]);
     }
     if (activeStep === 2) {
       setCinema();
+      setDate();
+      setShowTime();
+      setBookedSeats([]);
+    }
+
+    if (activeStep === 3) {
+      setDate();
+      setShowTime();
+      setBookedSeats([]);
+    }
+
+    if (activeStep === 4) {
+      setBookedSeats([]);
     }
   };
 
@@ -81,6 +158,7 @@ const BookingDialog = ({ open, handleClose, movie }) => {
         // onClose={handleClose}
         aria-labelledby="customized-dialog-title"
         open={open}
+        maxWidth="1000px"
       >
         <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
           Book Tickets
@@ -192,28 +270,250 @@ const BookingDialog = ({ open, handleClose, movie }) => {
                   </Box>
                 )}
                 {activeStep === 2 && (
-                  <Box marginTop={4} marginBottom={4} marginLeft={5}>
-                    <Typography
-                      marginTop={2}
-                      marginBottom={2}
-                      fontWeight="bold"
-                      variant="h6"
-                      color="primary"
-                    >
-                      Select Date and Time
-                    </Typography>
-                    <Typography
-                      marginTop={2}
-                      marginBottom={2}
-                      fontWeight="bold"
-                      variant="h6"
-                      color="primary"
-                    >
-                      Pick Seats
-                    </Typography>
+                  <Box
+                    display={"flex"}
+                    justifyContent={"center"}
+                    marginTop={4}
+                    marginBottom={4}
+                    gap={5}
+                  >
+                    <div>
+                      <Typography
+                        marginTop={2}
+                        marginBottom={2}
+                        fontWeight="bold"
+                        variant="h6"
+                        color="primary"
+                      >
+                        Select Date
+                      </Typography>
+                      <LocalizationProvider
+                        marginTop={2}
+                        marginBottom={2}
+                        dateAdapter={AdapterDayjs}
+                      >
+                        <DatePicker
+                          label="Select Date"
+                          disablePast
+                          maxDate={tomorrow}
+                          onChange={(newValue) => setDate(newValue)}
+                          open={openDate}
+                          onClose={() => setOpenDate(false)}
+                          slotProps={{
+                            textField: {
+                              onClick: () => setOpenDate(true),
+                            },
+                            openPickerIcon: {
+                              onClick: () => setOpenDate(true),
+                            },
+                            field: {
+                              readOnly: true,
+                            },
+                          }}
+                        />
+                      </LocalizationProvider>
+                    </div>
+                    {date && (
+                      <div>
+                        <Typography
+                          marginTop={2}
+                          marginBottom={2}
+                          fontWeight="bold"
+                          variant="h6"
+                          color="primary"
+                        >
+                          Show Timing
+                        </Typography>
+                        <FormControl>
+                          <RadioGroup
+                            aria-labelledby="demo-radio-buttons-group-label"
+                            name="radio-buttons-group"
+                            onChange={(event) =>
+                              setShowTime(event.target.value)
+                            }
+                          >
+                            {timings.map((time) => {
+                              return (
+                                <FormControlLabel
+                                  value={time}
+                                  control={<Radio />}
+                                  label={time}
+                                />
+                              );
+                            })}
+                          </RadioGroup>
+                        </FormControl>
+                      </div>
+                    )}
                   </Box>
                 )}
                 {activeStep === 3 && (
+                  <Box
+                    display={"flex"}
+                    flexDirection={"column"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    marginTop={3}
+                  >
+                    <Box
+                      display={"flex"}
+                      justifyContent={"center"}
+                      alignItems={"center"}
+                      gap={5}
+                    >
+                      {seatAvailability.map((seat) => {
+                        return (
+                          <Box
+                            display={"flex"}
+                            justifyContent={"center"}
+                            alignItems={"center"}
+                            gap={1}
+                          >
+                            <Box
+                              sx={{
+                                backgroundColor: `${seat.color}`,
+                                width: "15px",
+                                height: "15px",
+                              }}
+                            ></Box>
+                            <Typography
+                              fontWeight="bold"
+                              variant="body1"
+                              color="primary"
+                            >
+                              {seat.condition}
+                            </Typography>
+                          </Box>
+                        );
+                      })}
+                    </Box>
+                    <Box
+                      display={"flex"}
+                      alignItems={"center"}
+                      justifyContent={"center"}
+                      marginTop={0}
+                      marginBottom={0}
+                      sx={{ maxWidth: "600px" }}
+                    >
+                      <FormGroup
+                        row
+                        sx={{
+                          justifyContent: "center",
+                          width: "100%",
+                        }}
+                        onChange={seatBookingHandler}
+                      >
+                        <Divider sx={{ width: "100%", mt: "15px", mb: "15px" }}>
+                          <Typography variant="body1" sx={{ color: "#F44336" }}>
+                            REGULAR
+                          </Typography>
+                          <Typography variant="body1" sx={{ color: "#85BB65" }}>
+                            Rs. 250
+                          </Typography>
+                        </Divider>
+                        {Array.from({ length: 50 }, (_, index) => {
+                          if (index + 1 <= 24) {
+                            return (
+                              <FormControlLabel
+                                key={index}
+                                labelPlacement="bottom"
+                                label={`R-${index + 1}`}
+                                value={`R-${index + 1}`}
+                                control={
+                                  <Checkbox
+                                    icon={<EventSeatIcon color="error" />}
+                                    checkedIcon={
+                                      <EventSeatIcon color="warning" />
+                                    }
+                                  />
+                                }
+                              />
+                            );
+                          }
+
+                          if (index + 1 === 25) {
+                            return (
+                              <Divider
+                                sx={{ width: "100%", mt: "15px", mb: "15px" }}
+                              >
+                                <Typography
+                                  variant="body1"
+                                  sx={{ color: "#FFD700" }}
+                                >
+                                  GOLD
+                                </Typography>
+                                <Typography
+                                  variant="body1"
+                                  sx={{ color: "#85BB65" }}
+                                >
+                                  Rs. 450
+                                </Typography>
+                              </Divider>
+                            );
+                          }
+
+                          if (index + 1 > 24 && index + 1 <= 41) {
+                            return (
+                              <FormControlLabel
+                                key={index}
+                                labelPlacement="bottom"
+                                label={`G-${index - 25 + 1}`}
+                                value={`G-${index - 25 + 1}`}
+                                control={
+                                  <Checkbox
+                                    icon={<WeekendIcon color="error" />}
+                                    checkedIcon={
+                                      <WeekendIcon color="warning" />
+                                    }
+                                  />
+                                }
+                              />
+                            );
+                          }
+
+                          if (index + 1 === 42) {
+                            return (
+                              <Divider
+                                sx={{ width: "100%", mt: "15px", mb: "15px" }}
+                              >
+                                <Typography
+                                  variant="body1"
+                                  sx={{ color: "#E5E4E2" }}
+                                >
+                                  PLATINUM
+                                </Typography>
+                                <Typography
+                                  variant="body1"
+                                  sx={{ color: "#85BB65" }}
+                                >
+                                  Rs. 600
+                                </Typography>
+                              </Divider>
+                            );
+                          }
+
+                          if (index + 1 > 40) {
+                            return (
+                              <FormControlLabel
+                                key={index}
+                                labelPlacement="bottom"
+                                label={`P-${index - 42 + 1}`}
+                                value={`P-${index - 42 + 1}`}
+                                control={
+                                  <Checkbox
+                                    icon={<ChairIcon color="error" />}
+                                    checkedIcon={<ChairIcon color="warning" />}
+                                  />
+                                }
+                              />
+                            );
+                          }
+                        })}
+                      </FormGroup>
+                    </Box>
+                  </Box>
+                )}
+                {activeStep === 4 && (
                   <Typography sx={{ mt: 2, mb: 1 }}>
                     Step {activeStep}
                   </Typography>
@@ -233,6 +533,10 @@ const BookingDialog = ({ open, handleClose, movie }) => {
                   ) : activeStep === 0 && city ? (
                     <Button onClick={handleNext}>Next</Button>
                   ) : activeStep === 1 && cinema ? (
+                    <Button onClick={handleNext}>Next</Button>
+                  ) : activeStep === 2 && date && showTime ? (
+                    <Button onClick={handleNext}>Next</Button>
+                  ) : activeStep === 3 && bookedSeats.length !== 0 ? (
                     <Button onClick={handleNext}>Next</Button>
                   ) : null}
                 </Box>
