562b9ad944cd8e0441a33991f00187719a69c9ed
Yadu Krishnan <79215964+YaduKdev@users.noreply.github.com>
2026-01-29 12:42:49 +0530

    change way to get sessionid in booking success route and make build

--- src/Routes/BookingStatus/BookingSuccess.jsx
+++ src/Routes/BookingStatus/BookingSuccess.jsx
@@ -32,8 +32,10 @@ const BookingSuccess = () => {
   };
 
   useEffect(() => {
-    let sessionQuery = `${searchParams}`;
-    let sessionId = sessionQuery.replace("session_id=", "");
+    // let sessionQuery = `${searchParams}`;
+    // let sessionId = sessionQuery.replace("session_id=", "");
+    const [searchParams] = useSearchParams();
+    const sessionId = searchParams.get("session_id");
     const movieID = localStorage.getItem("movie");
 
     if (!sessionId || !movieID) {
